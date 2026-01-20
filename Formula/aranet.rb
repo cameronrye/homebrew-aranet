d30562b613933e70e9b23d9bae864d73be737788
Cameron Rye <c@meron.io>
2026-01-19 23:15:20 -0500

    Update aranet formula to v0.1.8

--- Formula/aranet.rb
+++ Formula/aranet.rb
@@ -1,10 +1,5 @@
 # Homebrew formula for Aranet CLI
-# This file serves as a template for the cameronrye/homebrew-aranet tap.
-#
-# To set up the tap repository:
-#   1. Create repo: github.com/cameronrye/homebrew-aranet
-#   2. Copy this file to Formula/aranet.rb in that repo
-#   3. Update SHA256 checksums after each release
+# This file is automatically updated by the release workflow.
 #
 # Users will install with:
 #   brew tap cameronrye/aranet
@@ -21,47 +16,24 @@ class Aranet < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/cameronrye/aranet/releases/download/v#{version}/aranet-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MACOS_ARM64"
+      url "https://github.com/cameronrye/aranet/releases/download/v0.1.8/aranet-cli-aarch64-apple-darwin.tar.xz"
+      sha256 "ee54d88ddd2de98bab1e27a468cd4e14fb758dd1fe75ba4216eac848c8bd8022"
     end
     on_intel do
-      url "https://github.com/cameronrye/aranet/releases/download/v#{version}/aranet-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MACOS_X64"
+      url "https://github.com/cameronrye/aranet/releases/download/v0.1.8/aranet-cli-x86_64-apple-darwin.tar.xz"
+      sha256 "dedaea9286b28ed4a863197c49b17024dbcb14240a4353d9d42e625fc6915953"
     end
   end
 
   on_linux do
-    on_arm do
-      url "https://github.com/cameronrye/aranet/releases/download/v#{version}/aranet-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
-    end
     on_intel do
-      url "https://github.com/cameronrye/aranet/releases/download/v#{version}/aranet-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_X64"
+      url "https://github.com/cameronrye/aranet/releases/download/v0.1.8/aranet-cli-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "e122d59f9437c7522d263ce67aa9592e39b600f63603918b1c21c8f1f8e73397"
     end
   end
 
-  # Linux requires D-Bus for Bluetooth
-  depends_on "dbus" => :build if OS.linux?
-
   def install
     bin.install "aranet"
-    
-    # Install shell completions if present in the archive
-    if File.exist?("completions/aranet.bash")
-      bash_completion.install "completions/aranet.bash" => "aranet"
-    end
-    if File.exist?("completions/aranet.zsh")
-      zsh_completion.install "completions/aranet.zsh" => "_aranet"
-    end
-    if File.exist?("completions/aranet.fish")
-      fish_completion.install "completions/aranet.fish"
-    end
-    
-    # Generate completions if not present
-    unless File.exist?("completions")
-      generate_completions_from_executable(bin/"aranet", "completions")
-    end
   end
 
   def caveats
@@ -72,17 +44,10 @@ class Aranet < Formula
         Grant Bluetooth permission when prompted, or add Terminal/iTerm
         to System Preferences > Privacy & Security > Bluetooth.
 
-      On Linux:
-        Ensure your user is in the 'bluetooth' group:
-          sudo usermod -aG bluetooth $USER
-        
-        You may need to log out and back in for changes to take effect.
-
       Quick start:
         aranet scan              # Find nearby devices
         aranet read              # Read current values
         aranet watch             # Continuous monitoring
-        aranet tui               # Interactive dashboard
     EOS
   end
 
@@ -92,9 +57,6 @@ class Aranet < Formula
     
     # Test version output
     assert_match version.to_s, shell_output("#{bin}/aranet --version")
-    
-    # Test that config command works (doesn't require Bluetooth)
-    system "#{bin}/aranet", "config", "path"
   end
 end
 
